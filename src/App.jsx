39df45626fa065f2b45a92104d6d5daefb1f33c7
douglasmnegri <douglasmnegri@gmail.com>
2024-11-03 20:55:04 -0300

    add a form to fill personal information

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,50 @@
 import { useState } from "react";
 
+import PersonalDetails from "./ProfileInfo";
+
 function SignUpForm() {
   return (
     <div>
-      <h1 className="text-red-600">Test</h1>
-      <div className="border-solid border-2 border-black">Details</div>
+      <h1>Personal Information</h1>
+      <div className="border-2 border-gray-300 bg-gray-800 bg-opacity-80 w-[800px] h-[600px] shadow-md">
+        <div className="flex justify-around m-2 p-2 gap-2">
+          <div className="personal-details">
+            <PersonalDetails
+              title="Full Name"
+              placeholder="Douglas"
+              type="text"
+              width="w-[350px]"
+            />
+            <PersonalDetails
+              title="Job Title"
+              placeholder="Software Engineer"
+              type="text"
+            />
+          </div>
+          <div className="personal-details">
+            <PersonalDetails
+              title="Email"
+              placeholder="johndoe@mail.com"
+              type="email"
+            />
+            <PersonalDetails
+              title="Phone"
+              placeholder="456 956 232"
+              type="tel"
+            />
+          </div>
+        </div>
+        <div className="flex justify-start ml-5 p-2 gap-2">
+          <PersonalDetails
+            title="City"
+            placeholder="New York"
+            type="text"
+            width="w-[740px]"
+          />
+        </div>
+      </div>
     </div>
   );
 }
 
-
-export default SignUpForm
+export default SignUpForm;
---
+++ src/ProfileInfo.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+export default function PersonalDetails({ title, placeholder, type, width }) {
+  const [input, setInput] = useState("");
+
+  function handleChange(e) {
+    setInput(e.target.value);
+  }
+
+  return (
+    <label className="text-l">
+      {title}
+      <input
+        type={type}
+        value={input}
+        onChange={handleChange}
+        placeholder={placeholder}
+        className={`border-solid border-white border-2 block p-2 ${width || "w-[350px]"}`}
+        />
+    </label>
+  );
+}
