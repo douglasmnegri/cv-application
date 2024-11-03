58b10607b508844a67643f40a84197b9d9623c16
douglasmnegri <douglasmnegri@gmail.com>
2024-11-03 19:34:51 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function SignUpForm() {
+  return (
+    <div>
+      <h1 className="text-red-600">Test</h1>
+      <div className="border-solid border-2 border-black">Details</div>
+    </div>
+  );
+}
+
+
+export default SignUpForm
