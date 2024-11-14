bd40d4870709417a8de8cc013cdcf6c5ef402fde
Douglas Melo <douglasmnegri@gmail.com>
2024-11-13 21:27:10 -0300

    style curriculum template

--- src/html_components/CurriculumTemplate.jsx
+++ src/html_components/CurriculumTemplate.jsx
@@ -1,17 +1,17 @@
 function CurriculumTemplate() {
   return (
-    <div className="flex justify-center py-10 bg-gray-100 min-h-screen">
-      <div className="w-4/5 h-full max-w-4xl shadow-lg border border-gray-200 bg-white">
-        <div className="grid grid-rows-3">
-          <div className="bg-pale-yellow text-black p-6 row-span-1 flex justify-center items-center">
-            <div>
-              <h1 className="text-4xl font-bold mb-8 text-black mr-12">
-                LISANDRO MILANESI
+    <div className="flex justify-center bg-gray-100 p-4">
+      <div className="w-full max-w-4xl shadow-lg border border-gray-200 bg-white">
+        <div className="grid grid-rows-4 h-full">
+          <div className="bg-pale-yellow text-black p-6 row-span-1 flex justify-around items-center">
+            <div className="mr-14 w-[650px]">
+              <h1 className="text-5xl font-bold text-black	leading-snug	 text-center">
+                Douglas M. Negri
               </h1>
             </div>
-            <div className="text-sm">
+            <div className="text-sm text-center">
               <h2 className="text-black font-bold uppercase w-4/5">Profile</h2>
-              <p className="mt-4">
+              <p className="mt-4 w-4/5">
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                 Officia necessitatibus aut accusamus! Adipisci voluptas cumque
                 deserunt delectus repellat temporibus voluptates nam vel nisi
@@ -20,30 +20,36 @@ function CurriculumTemplate() {
             </div>
           </div>
 
-          <div className="grid grid-cols-3 bg-light-yellow p-8 text-black row-span-2">
+          <div className="grid grid-cols-3 bg-light-yellow px-6 py-12 text-black row-span-4">
             <div className="space-y-4 text-md col-span-1">
-              <div className="font-bold uppercase">Languages</div>
+              <div className="font-bold uppercase border-brown-500 border-b-4">
+                Languages
+              </div>
               <ul className="list-disc pl-4 p-8">
                 <li>English</li>
                 <li>Portuguese</li>
                 <li>Spanish</li>
               </ul>
 
-              <div className="font-bold uppercase mt-8">Key Skills</div>
+              <div className="font-bold uppercase mt-8 border-brown-500 border-b-4">
+                Key Skills
+              </div>
               <ul className="list-disc pl-4">
                 <li>Budget management</li>
                 <li>Excellent listener</li>
-                <li>Friendly, courteous, service-oriented</li>
+                <li>Friendly & courteous</li>
                 <li>Poised under pressure</li>
                 <li>Staff training & coaching</li>
                 <li>Recruiting & hiring talent</li>
                 <li>Quality assurance</li>
-                <li>Solid written & verbal communicator</li>
+                <li>Verbal communicator</li>
               </ul>
             </div>
 
-            <div className="col-span-2 ml-4">
-              <div className="text-lg font-bold uppercase">Work Experience</div>
+            <div className="col-span-2 ml-14">
+              <div className="font-bold uppercase border-solid border-brown-500 border-b-4">
+                Work Experience
+              </div>
               <div className="mt-4 space-y-6">
                 <div>
                   <div className="font-semibold">Assistant Hotel Manager</div>
@@ -54,6 +60,18 @@ function CurriculumTemplate() {
                     Eligendi incidunt numquam ipsum omnis officiis atque
                     obcaecati asperiores? Et, eius beatae consequatur laborum
                     ut, architecto dolores quidem iste sed sint eligendi!
+                  </p>
+                </div>
+
+                <div>
+                  <div className="font-semibold">Assistant Hotel Manager</div>
+                  <div>The Seattle Sea Home | Seattle, WA</div>
+                  <div>20XX – 20XX</div>
+                  <p className="mt-2">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Fuga labore, possimus quam saepe enim error ea mollitia
+                    impedit praesentium cumque maiores nostrum. Accusamus sequi
+                    ab placeat itaque nostrum ea quia!
                   </p>
                 </div>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,18 @@ module.exports = {
       colors: {
         "pale-yellow": "#f7f2e6",
         "light-yellow": "#faf8ef",
+        brown: {
+          50: "#fdf8f6",
+          100: "#f2e8e5",
+          200: "#eaddd7",
+          300: "#e0cec7",
+          400: "#d2bab0",
+          500: "#bfa094",
+          600: "#a18072",
+          700: "#977669",
+          800: "#846358",
+          900: "#43302b",
+        },
       },
     },
   },
